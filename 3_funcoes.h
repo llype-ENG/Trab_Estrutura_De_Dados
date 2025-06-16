28501484f41d13f6f1968cde29e1aeffe704eb37
Juliano-Arthur <163357784+Juliano-Arthur@users.noreply.github.com>
2025-06-16 19:53:01 -0300

    Update 3_funcoes.h
    Adicionando Struct e funções criando uma pilha, limpar pilha, adicionar disco no topo e remover disco do topo

--- 3_funcoes.h
+++ 3_funcoes.h
@@ -1,5 +1,17 @@
 #ifndef TORRE_HANOI_H
 #define TORRE_HANOI_H
+typedef struct {
+    int* discos;
+    int topo;
+    int capacidade;
+} Pilha;
 
+extern Pilha torres[3];
+extern int movimentosFeitos;
+
+void criandoUmaPilha(Pilha* p, int capacidade);
+void limparPilha(Pilha* p);
+int adicionarDiscoNoTopo(Pilha* p, int valor);
+int removerDoTopo(Pilha* p, int* valor);
 
 #endif
